2b2968ee2c906db6eeba487432f70e6a3bab0b94
mian0606 <mih037@ucsd.edu>
2026-05-15 19:25:48 -0700

    on load

--- map.js
+++ map.js
@@ -105,119 +105,123 @@ map.on('load', async () => {
   map.on('resize', updatePositions); // Update on window resize
   map.on('moveend', updatePositions); // Final adjustment after movement ends
 
+
+
+  function formatTime(minutes) {
+    const date = new Date(0, 0, 0, 0, minutes);
+    return date.toLocaleString('en-US', { timeStyle: 'short' });
+  }
+  const timeSlider = document.getElementById('time-slider');
+  const selectedTime = document.getElementById('selected-time');
+  const anyTimeLabel = document.getElementById('any-time');
+
+
+  function updateTimeDisplay() {
+    const timeFilter = Number(timeSlider.value);
+
+    if (timeFilter === -1) {
+      selectedTime.textContent = '';
+      anyTimeLabel.style.display = 'block';
+    } else {
+      selectedTime.textContent = formatTime(timeFilter);
+      anyTimeLabel.style.display = 'none';
+    }
+
+    updateScatterPlot(timeFilter);
+  }
+
+
+
+  function minutesSinceMidnight(date) {
+    return date.getHours() * 60 + date.getMinutes();
+  }
+
+  function filterTripsbyTime(trips, timeFilter) {
+    return timeFilter === -1
+      ? trips
+      : trips.filter((trip) => {
+          const startedMinutes = minutesSinceMidnight(trip.started_at);
+          const endedMinutes = minutesSinceMidnight(trip.ended_at);
+
+          return (
+            Math.abs(startedMinutes - timeFilter) <= 60 ||
+            Math.abs(endedMinutes - timeFilter) <= 60
+          );
+        });
+  }
+
+  let trips = await d3.csv(
+    'https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv',
+    (trip) => {
+      trip.started_at = new Date(trip.started_at);
+      trip.ended_at = new Date(trip.ended_at);
+      return trip;
+    },
+  );
+
+
+
+  function computeStationTraffic(stations, trips) {
+    const departures = d3.rollup(
+      trips,
+      (v) => v.length,
+      (d) => d.start_station_id,
+    );
+
+    const arrivals = d3.rollup(
+      trips,
+      (v) => v.length,
+      (d) => d.end_station_id,
+    );
+
+    return stations.map((station) => {
+      let id = station.short_name;
+
+      station.departures = departures.get(id) ?? 0;
+      station.arrivals = arrivals.get(id) ?? 0;
+      station.totalTraffic = station.departures + station.arrivals;
+
+      return station;
+    }); 
+  }
+
+
+  let stations = jsonData.data.stations;
+  stations = computeStationTraffic(stations, trips);
+  const radiusScale = d3
+    .scaleSqrt()
+    .domain([0, d3.max(stations, d => d.totalTraffic)])
+    .range([0, 25]);
+
+  circles
+    .attr('r', d => radiusScale(d.totalTraffic))
+    .append('title')
+    .text(d =>
+      `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`
+    );
+
+
+
+
+  function updateScatterPlot(timeFilter) {
+    const filteredTrips = filterTripsbyTime(trips, timeFilter);
+    const filteredStations = computeStationTraffic(stations, filteredTrips);
+
+    timeFilter === -1
+      ? radiusScale.range([0, 25])
+      : radiusScale.range([3, 50]);
+
+    circles
+      .data(filteredStations, (d) => d.short_name)
+      .join('circle')
+      .attr('r', (d) => radiusScale(d.totalTraffic));
+  }
+
+
+  timeSlider.addEventListener('input', updateTimeDisplay);
+  updateTimeDisplay();
+
 });
 
 
-function formatTime(minutes) {
-  const date = new Date(0, 0, 0, 0, minutes);
-  return date.toLocaleString('en-US', { timeStyle: 'short' });
-}
-const timeSlider = document.getElementById('time-slider');
-const selectedTime = document.getElementById('selected-time');
-const anyTimeLabel = document.getElementById('any-time');
 
-
-function updateTimeDisplay() {
-  const timeFilter = Number(timeSlider.value);
-
-  if (timeFilter === -1) {
-    selectedTime.textContent = '';
-    anyTimeLabel.style.display = 'block';
-  } else {
-    selectedTime.textContent = formatTime(timeFilter);
-    anyTimeLabel.style.display = 'none';
-  }
-
-  updateScatterPlot(timeFilter);
-}
-
-
-
-function minutesSinceMidnight(date) {
-  return date.getHours() * 60 + date.getMinutes();
-}
-
-function filterTripsbyTime(trips, timeFilter) {
-  return timeFilter === -1
-    ? trips
-    : trips.filter((trip) => {
-        const startedMinutes = minutesSinceMidnight(trip.started_at);
-        const endedMinutes = minutesSinceMidnight(trip.ended_at);
-
-        return (
-          Math.abs(startedMinutes - timeFilter) <= 60 ||
-          Math.abs(endedMinutes - timeFilter) <= 60
-        );
-      });
-}
-
-let trips = await d3.csv(
-  'https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv',
-  (trip) => {
-    trip.started_at = new Date(trip.started_at);
-    trip.ended_at = new Date(trip.ended_at);
-    return trip;
-  },
-);
-
-
-
-function computeStationTraffic(stations, trips) {
-  const departures = d3.rollup(
-    trips,
-    (v) => v.length,
-    (d) => d.start_station_id,
-  );
-
-  const arrivals = d3.rollup(
-    trips,
-    (v) => v.length,
-    (d) => d.end_station_id,
-  );
-
-  return stations.map((station) => {
-    let id = station.short_name;
-
-    station.departures = departures.get(id) ?? 0;
-    station.arrivals = arrivals.get(id) ?? 0;
-    station.totalTraffic = station.departures + station.arrivals;
-
-    return station;
-  });
-}
-
-
-let stations = jsonData.data.stations;
-stations = computeStationTraffic(stations, trips);
-const radiusScale = d3
-  .scaleSqrt()
-  .domain([0, d3.max(stations, d => d.totalTraffic)])
-  .range([0, 25]);
-
-circles
-  .attr('r', d => radiusScale(d.totalTraffic))
-  .append('title')
-  .text(d =>
-    `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`
-  );
-
-
-
-
-function updateScatterPlot(timeFilter) {
-  const filteredTrips = filterTripsbyTime(trips, timeFilter);
-  const filteredStations = computeStationTraffic(stations, filteredTrips);
-
-  timeFilter === -1
-    ? radiusScale.range([0, 25])
-    : radiusScale.range([3, 50]);
-
-  circles
-    .data(filteredStations, (d) => d.short_name)
-    .join('circle')
-    .attr('r', (d) => radiusScale(d.totalTraffic));
-}
-
-
-timeSlider.addEventListener('input', updateTimeDisplay);
-updateTimeDisplay();
